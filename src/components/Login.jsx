e0a54e03e89934b61b81aa5b54d3c578a88d93b4
Asheq mahmud <128743584+noobhacker32003@users.noreply.github.com>
2024-11-08 12:03:28 +0600

    first one

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            code here
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            code here
+        </div>
+    );
+};
+
+export default Register;
